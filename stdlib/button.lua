f67a1b38acb00afdb33e268eb53156a23c37dfd6
Mitja Felicijan <mitja.felicijan@gmail.com>
2025-08-11 20:38:41 +0200

    Cleanup of old button definitions

--- stdlib/button.lua
+++ stdlib/button.lua
@@ -1,22 +1,9 @@
 local button = {}
 
--- button.PAD_UP    = { keyboard = 87, xbox = 1,  playstation = 1  }  -- Key: W
--- button.PAD_DOWN  = { keyboard = 83, xbox = 3,  playstation = 2  }  -- Key: S
--- button.PAD_LEFT  = { keyboard = 65, xbox = 4,  playstation = 3  }  -- Key: A
--- button.PAD_RIGHT = { keyboard = 68, xbox = 2,  playstation = 4  }  -- Key: D
-
--- button.A         = { keyboard = 76, xbox = 7,  playstation = 7  }  -- Key: L
--- button.B         = { keyboard = 80, xbox = 6,  playstation = 6  }  -- Key: P
--- button.X         = { keyboard = 75, xbox = 8,  playstation = 8  }  -- Key: K
--- button.Y         = { keyboard = 79, xbox = 5,  playstation = 5  }  -- Key: O
-
--- button.SELECT    = { keyboard = 81, xbox = 13, playstation = 13 }  -- Key: Q
--- button.START     = { keyboard = 69, xbox = 15, playstation = 15 }  -- Key: E
-
-button.PAD_UP    = { keyboard = 265, xbox = 1,  playstation = 1  }  -- Key: Up arrow
-button.PAD_DOWN  = { keyboard = 264, xbox = 3,  playstation = 2  }  -- Key: Down arrow
-button.PAD_LEFT  = { keyboard = 263, xbox = 4,  playstation = 3  }  -- Key: Left arrow
-button.PAD_RIGHT = { keyboard = 262, xbox = 2,  playstation = 4  }  -- Key: Right arrow
+button.PAD_UP    = { keyboard = 265, xbox = 1,  playstation = 1  }  -- Key: Arrow Up
+button.PAD_DOWN  = { keyboard = 264, xbox = 3,  playstation = 2  }  -- Key: Arrow Down
+button.PAD_LEFT  = { keyboard = 263, xbox = 4,  playstation = 3  }  -- Key: Arrow Left
+button.PAD_RIGHT = { keyboard = 262, xbox = 2,  playstation = 4  }  -- Key: Arrow Right
 
 button.A         = { keyboard = 65,  xbox = 7,  playstation = 7  }  -- Key: A
 button.B         = { keyboard = 83,  xbox = 6,  playstation = 6  }  -- Key: S
